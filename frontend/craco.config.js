ad51c03d66bda65d414ecbab50e194ef85fcece4
Marcos Gonzalez <xdmgzdev@gmail.com>
2026-01-10 18:54:21 -0500

    change craco.config

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -71,7 +71,7 @@ const webpackConfig = {
 };
 
 // Only add babel metadata plugin during dev server
-if (process.env.NODE_ENV === "development" && config.enableVisualEdits && babelMetadataPlugin) {
+if (config.enableVisualEdits && babelMetadataPlugin) {
   webpackConfig.babel = {
     plugins: [babelMetadataPlugin],
   };
